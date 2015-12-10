7673adaca8c42d63e5f669679c56ebdffd555b40
Alex Tiller <tilleao@dshs.wa.gov>
2015-12-10 08:50:00 -0800

    First Commit

---
+++ demoApp.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('TableSorterDemo', ['twrSortableTable'])
+        .controller('TableSorterController', tableSorterController);
+
+
+    function tableSorterController() {
+        var vm = this;
+
+        vm.settings = {
+            showStateTableCode: false,
+            showGameTableCode: false,
+            showJsonData: false
+        }
+        vm.toggleStateTableCode = toggleStateTableCode;
+        vm.toggleGameTableCode = toggleGameTableCode;
+        vm.toggleJsonData = toggleJsonData;
+
+        vm.states = [
+            { name: 'Washington', capital: 'Olympia', nickname: 'The Evergreen State', statehoodDate: new Date(1889, 11, 11) },
+            { name: 'New York', capital: 'Albany', nickname: 'Empire State', statehoodDate: new Date(1788, 7, 26) },
+            { name: 'California', capital: 'Sacramento', nickname: 'Golden State', statehoodDate: new Date(1850, 9, 9) },
+            { name: 'Texas', capital: 'Austin', nickname: 'Lone Star State', statehoodDate: new Date(1845, 12, 29) },
+        ]
+
+        vm.games = [
+            { name: 'Super Metroid', platform: 'SNES', releaseDate: new Date(1994, 4, 18) },
+            { name: 'Fallout 2', platform: 'Windows', releaseDate: new Date(1998, 9, 30) },
+            { name: 'The Legend of Zelda', platform: 'NES', releaseDate: new Date(1987, 8, 22) },
+            { name: 'Master of Orion 2', platform: 'Windows', releaseDate: new Date(1996, 10, 31) }
+        ]
+
+        function toggleStateTableCode() {
+            vm.settings.showStateTableCode = !vm.settings.showStateTableCode;
+        }
+        function toggleGameTableCode() {
+            vm.settings.showGameTableCode = !vm.settings.showGameTableCode;
+        }
+        function toggleJsonData() {
+            vm.settings.showJsonData = !vm.settings.showJsonData;
+        }
+    }
+})();
---
+++ src/twrSortableTable.js
@@ -0,0 +1,68 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('twrSortableTable', [])
+        .directive('twrSortableTable', twrSortableTable)
+        .directive('twrTableSorter', twrTableSorter);
+
+    function twrSortableTable() {
+        return {
+            restrict: 'A',
+            scope: {
+                twrSortingVariable: '='
+            },
+            controller: ['$scope', function (scope) {
+                scope.headers = [];
+
+                return {
+                    setHeader: setHeader,
+                    changeSorting: changeSorting
+                }
+
+                function setHeader(headerElement) {
+                    scope.headers.push(headerElement);
+                }
+
+                function changeSorting(propertyName) {
+                    angular.forEach(scope.headers, function (header) {
+                        header.removeClass('twr-sort-asc');
+                        header.removeClass('twr-sort-desc');
+                    });
+                    scope.twrSortingVariable = propertyName;
+                }
+            }]
+        }
+    }
+
+    function twrTableSorter() {
+        return {
+            require: '^twrSortableTable',
+            restrict: 'A',
+            link: function (scope, element, attrs, tableController) {
+                element.bind('click', function () {
+                    var propertyName = attrs.twrTableSorter;
+
+                    if (element.hasClass('twr-sort-desc')) {
+                        tableController.changeSorting('-' + propertyName);
+                        element.addClass('twr-sort-asc');
+                        scope.$apply();
+                    }
+                    else {
+                        tableController.changeSorting(propertyName);
+                        element.addClass('twr-sort-desc');
+                        scope.$apply();
+                    }
+                });
+
+                tableController.setHeader(element);
+
+                if (attrs.twrTableDefault != null && attrs.twrTableDefault.length >= 0) {
+                    tableController.changeSorting(attrs.twrTableSorter);
+                    element.addClass('twr-sort-desc');
+                }
+                element.append('<i class="fa fa-sort"></i><i class="fa fa-sort-desc"></i><i class="fa fa-sort-asc"></i>');
+            }
+        }
+    }
+})();
